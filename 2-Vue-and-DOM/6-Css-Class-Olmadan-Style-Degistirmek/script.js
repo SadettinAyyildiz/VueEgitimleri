71f0391fef8c9cca62e6f527c390a5082d11521e
user.email <ayyildiz.s83@gmail.com>
2020-10-22 21:22:59 +0300

    DOM tamamlandi

---
+++ 2-Vue-and-DOM/6-Css-Class-Olmadan-Style-Degistirmek/script.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#app",
+    data: {
+        color: "green",
+        height: 100
+    },
+    computed: {
+        customStyle: function() {
+            return {
+                backgroundColor: this.color,
+                height: this.height + "px"
+            }
+        }
+    },
+
+})
