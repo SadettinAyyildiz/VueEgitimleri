67e8082e8536ec84cdf7666d0b29fed69481b2fb
user.email <ayyildiz.s83@gmail.com>
2020-10-26 00:10:52 +0300

    116 slot icerik gonderme tamamlandi

---
+++ 7-Gelismis-Component-Kullanimi/1-Slot-ile-Icerik-Gondermek/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <app-memory content="Bugün hava çok güze..."></app-memory>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Memory from './Memory';
+
+export default {
+  components: {
+    appMemory: Memory
+  }
+}
+</script>
---
+++ 7-Gelismis-Component-Kullanimi/1-Slot-ile-Icerik-Gondermek/src/Memory.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p class="text-center">{{ content }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['content']
+}
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px #666;
+        padding: 20px 20px;
+        margin-top: 20px;
+    }
+</style>
