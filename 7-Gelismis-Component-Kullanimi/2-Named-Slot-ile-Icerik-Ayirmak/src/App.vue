51292559e309e1d26edbd2bfacce150bb8b95202
user.email <ayyildiz.s83@gmail.com>
2020-10-26 00:34:18 +0300

    slotlar tamamlandi

---
+++ 7-Gelismis-Component-Kullanimi/2-Named-Slot-ile-Icerik-Ayirmak/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <app-memory>
+          <h3 slot="heading">Lorem, ipsum.</h3>
+          <p slot="content">Lorem ipsum dolor sit amet.</p>
+          <p>İsimlendirilmemiş slot.</p>
+        </app-memory>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Memory from './Memory';
+
+export default {
+  components: {
+    appMemory: Memory
+  }
+}
+</script>
---
+++ 7-Gelismis-Component-Kullanimi/2-Named-Slot-ile-Icerik-Ayirmak/src/Memory.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="memory">
+            <slot name="heading"></slot>
+        </div>
+        <div class="memory">
+            <slot name="content"></slot>
+        </div>
+        <div class="memory">
+            <slot></slot>
+        </div>
+        <div class="memory">
+            <slot name="author">
+                <p>Name'e veri gelmezse default/içindeki değeri yazar.</p>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['content']
+}
+</script>
+
+<style scoped>
+    div.memory {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px #666;
+        padding: 20px 20px;
+        margin-top: 20px;
+    }
+</style>
