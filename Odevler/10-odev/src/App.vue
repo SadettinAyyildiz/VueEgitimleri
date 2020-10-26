1a03a2bf67f0cef7ed4f84e57f8fbd507daca86e
user.email <ayyildiz.s83@gmail.com>
2020-10-27 01:02:03 +0300

    odev 10 tamamlandi

---
+++ Odevler/10-odev/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="col-md-6 col-md-offset-3">
+      <h3>Directive Sınavı</h3>
+
+      <button v-custom-on:click="clicked" class="btn btn-success">
+        Tıkla!
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  directives: {
+    customOn : {
+      bind(el, binding){
+        el.onclick = function(){
+          binding.value();
+        }
+      }
+    }
+  },
+  methods: {
+    clicked(){
+      alert('Tıklandı');
+    }
+  }
+}
+</script>
