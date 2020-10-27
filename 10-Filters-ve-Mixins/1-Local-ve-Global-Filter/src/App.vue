907a232ef07ced6e2c3eb72e2898705c385a2449
user.email <ayyildiz.s83@gmail.com>
2020-10-27 13:42:18 +0300

    ders 167 tamamlandi

---
+++ 10-Filters-ve-Mixins/1-Local-ve-Global-Filter/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h3>Filter ve Mixins</h3>
+        <p>{{ message | upper }}</p>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      message: "Hello world!!"
+    }
+  },
+  filters: {
+    upper(value){
+      return value.toUpperCase();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
