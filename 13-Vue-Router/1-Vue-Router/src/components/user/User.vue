19e8ef6583d7cafa7392c1fc6f01cad047a6c141
user.email <ayyildiz.s83@gmail.com>
2020-10-29 16:58:47 +0300

    ders 242 tamamlandi

--- 13-Vue-Router/1-Vue-Router/src/components/user/User.vue
+++ 13-Vue-Router/1-Vue-Router/src/components/user/User.vue
@@ -2,13 +2,29 @@
   <div class="container">
     <h3>User Component</h3>
     <hr>
+    <p>User ID: {{ id }}</p>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, earum libero odit optio repellendus sed
       soluta? Animi, atque blanditiis commodi consequatur distinctio dolorem eaque eos expedita, modi reprehenderit
       repudiandae soluta.
     </p>
+    <button class="btn btn-success" @click="navigatetoHome">Home Component</button>
   </div>
 </template>
 <script>
-  export default {}
+  export default {
+    data(){
+      return{
+        id: this.$route.params.id
+      }
+    },
+    methods: {
+      navigatetoHome(){
+        // aşağıdaki üç yöntemle de yapılabilir
+        //this.$router.push("/")
+        //this.$router.push({path : '/'})
+        this.$router.push({name : 'anasayfa'})
+      }
+    }
+  }
 </script>
