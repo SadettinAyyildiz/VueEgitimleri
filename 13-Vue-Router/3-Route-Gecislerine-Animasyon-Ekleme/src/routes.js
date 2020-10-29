01701311d0a24450f8d36b6179182cde36d52844
user.email <ayyildiz.s83@gmail.com>
2020-10-29 23:18:01 +0300

    Routing Tamamlandi - Hata Giderildi

--- 13-Vue-Router/3-Route-Gecislerine-Animasyon-Ekleme/src/routes.js
+++ 13-Vue-Router/3-Route-Gecislerine-Animasyon-Ekleme/src/routes.js
@@ -25,10 +25,6 @@ const UserEdit = resolve => {
   })
 }
 
-import User from "./components/user/User";
-import UserStart from "./components/user/UserStart";
-import UserDetail from "./components/user/UserDetail";
-import UserEdit from "./components/user/UserEdit";
 
 
 export const routes = [
